cd73f983c02ec093aab5958c8ccb7d0da9213862
Amir Alian <amircup2006@gmail.com>
2021-02-17 15:15:04 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ echo "Branch name: ${BRANCH_NAME}"
 echo "Pull request title: ${PULL_REQUEST_TITLE}"
 echo "**********************************************"
 
-IS_RELEASE_BRANCH=$( echo ${BRANCH_NAME} | grep -P "^(release|revert)/" | wc -l)
+IS_RELEASE_BRANCH=$( echo ${BRANCH_NAME} | grep -P "^(release|revert).*/" | wc -l)
 
 
 
