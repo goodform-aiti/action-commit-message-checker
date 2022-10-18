dfbfc1ce6f1271687381c84fb9a779ecf66dce95
Lubos Hubacek <lubos.hubacek@ateli.cz>
2022-10-18 23:51:01 +0200

    Better parsing of commit messages

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # action-commit-message-checker
+
+```shell
+$ ../action-commit-message-checker/entrypoint.sh
+Branch name: feature/GF-15
+Valid pull request prefix: GF-15
+**********************************************
+Commit message of '02e1a3d0' should be started with: 'GF-15', but it is started with 'meltdown'
+Commit message of '0a0292e7' should be started with: 'GF-15', but it is started with 'example commit message'
+How you can edit your commit messages? https://github.com/ateli-development/shipgratis/wiki/How-to-Change-a-Git-Commit-Message
+```
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
 
+# BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
+PREFIX_REGEX="[a-zA-Z]+-\d+"
+VALID_PREFIX=$(echo ${BRANCH_NAME} | grep -oP ${PREFIX_REGEX})
+
 echo "Branch name: ${BRANCH_NAME}"
-echo "Pull request title: ${PULL_REQUEST_TITLE}"
+echo "Valid pull request prefix: ${VALID_PREFIX}"
 echo "**********************************************"
 
-JIRA_PREFIX="GF"
-VALID_COMMIT_MESSAGE_PREFIX=$(echo ${BRANCH_NAME} | grep -oP "$JIRA_PREFIX-\d+")
+# 40 characters - commit hash
+GIT_MESSAGES=$(git log remotes/origin/master.. --no-merges --first-parent --pretty=format:"%H~%s")
+ERR_COUNT=0
 
-
-GIT_MESSAGES=$(git log remotes/origin/master.. --no-merges --first-parent --pretty=format:%H%s | grep -oP "^.{40}.{8}")
-for message in $GIT_MESSAGES
+IFS=$'\n'       # make newlines the only separator
+for MESSAGE in $GIT_MESSAGES
 do
-  COMMIT_REVISION_NUMBER=$(echo $message | cut -c 1-8)
-  COMMIT_MESSAGE_PREFIX=$(echo $message | cut -c 41-47)
+    COMMIT_REVISION_NUMBER=$(echo $MESSAGE | cut -c 1-8)
+    COMMIT_MESSAGE=$(echo $MESSAGE | cut -c 42-)
+    COMMIT_MESSAGE_PREFIX=$(echo $COMMIT_MESSAGE | grep -oP ${PREFIX_REGEX})
 
+    if [[ $COMMIT_MESSAGE_PREFIX != $VALID_PREFIX ]]
+    then
+        printf "Commit message of '%s' should be started with: '%s', but it is started with '%s'\n" $COMMIT_REVISION_NUMBER $VALID_PREFIX $COMMIT_MESSAGE
 
-  if [[ $VALID_COMMIT_MESSAGE_PREFIX != $COMMIT_MESSAGE_PREFIX ]]
-  then
-    printf "Commit message with revision '$COMMIT_REVISION_NUMBER' should be started with: '$VALID_COMMIT_MESSAGE_PREFIX', but it is started with '$COMMIT_MESSAGE_PREFIX'\n"
-    printf "How you can edit your commit messages?: https://github.com/ateli-development/shipgratis/wiki/How-to-Change-a-Git-Commit-Message"
-    exit 101
-  fi
+        ERR_COUNT=$ERR_COUNT+1
+    fi
 done
+
+if [[ $ERR_COUNT != 0 ]]
+then
+    printf "How you can edit your commit messages? https://github.com/ateli-development/shipgratis/wiki/How-to-Change-a-Git-Commit-Message\n"
+fi
